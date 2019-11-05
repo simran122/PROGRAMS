187d38488c2c4b40bb3b17ba767e5834d8de3d65
simran122 <54198569+simran122@users.noreply.github.com>
2019-11-05 17:48:46 +0530

    Create Greater of two number.c

---
+++ Greater of two number.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int a,b;
+printf("Enter Any 2 Number :");
+scanf("%d%d",&a,&b);
+if(a>b)
+{
+printf("a is greater ");
+}
+else
+{
+printf("b is greater ");
+}
+return 0;
+}
