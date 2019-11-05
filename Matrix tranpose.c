d78805cba00aabd8f6e88010be2729417ed5be64
simran122 <54198569+simran122@users.noreply.github.com>
2019-11-05 18:08:02 +0530

    Create Matrix tranpose.c

---
+++ Matrix tranpose.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+int a[10][10], b[10][10];
+int n,m,i,j;
+printf("Enter  Size Of Matrix a as m,n:");
+scanf("%d%d",&m,&n);
+printf("\nEnter Element Of Matrix A Row Wise \n",m&n);
+for(i=0;i<m;i++)
+{
+for(j=0;j<n;j++)
+{
+scanf("%d",&a[i][j]);
+}
+}
+for(i=0;i<m;i++)
+{
+for(j=0;j<n;j++)
+{
+b[j][i]=a[i][j];
+}
+}
+printf("\n Tranpose is \n\n");
+for(i=0;i<n;i++)
+{
+for(j=0;j<m;j++)
+{
+printf("%d ",b[i][j]);
+}
+printf("\n");
+                                                                    
+}
+}
