0c26f8fa9918da20fc41988524bec49a3193494f
simran122 <54198569+simran122@users.noreply.github.com>
2019-11-06 20:54:23 +0530

    Update Matrix tranpose.c

--- Matrix tranpose.c
+++ Matrix tranpose.c
@@ -5,7 +5,7 @@ int a[10][10], b[10][10];
 int n,m,i,j;
 printf("Enter  Size Of Matrix a as m,n:");
 scanf("%d%d",&m,&n);
-printf("\nEnter Element Of Matrix A Row Wise \n",m&n);
+printf("\nEnter Element Of Matrix A Row Wise \n",m,n);
 for(i=0;i<m;i++)
 {
 for(j=0;j<n;j++)
