b9f7bc1c921b2fd9130c6a315037c556eae812cf
simran122 <54198569+simran122@users.noreply.github.com>
2019-11-06 21:06:13 +0530

    Update Greatest of 3 number.c

--- Greatest of 3 number.c
+++ Greatest of 3 number.c
@@ -1,16 +1,14 @@
+  '''c                                                                                  
 #include<stdio.h>
-int main()
+void main()
 {
-int a,b;
-printf("Enter Any 2 Number :");
-scanf("%d%d",&a,&b);
-if(a>b)
-{
-printf("a is greater ");
-}
-else
-{
-printf("b is greater ");
-}
-return 0;
+int a,b,c;
+printf("enter the three numbers which you want to compare \n");
+scanf("%d%d%d",&a,&b,&c);
+if(a>b&&a>c)
+printf("\n%d is the greatest number\n",a);
+else if(b>a&&b>c)
+printf("\n%d is the greatest number\n",b);
+else 
+printf("\n%d is the greatest number\n",c);
 }
